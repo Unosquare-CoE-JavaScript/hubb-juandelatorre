5e96f32e541af467aa1630be29babf29729bfe3b
JP de la Torre <delatorre.juanpablo@gmail.com>
2021-08-06 21:06:13 -0400

    Vanilla version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+export const routes: Routes = [
+  {
+    path: 'examples',
+    loadChildren: () =>
+      import('./examples/examples.module').then(m => m.ExamplesModule),
+  },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./main/main.module').then(m => m.MainModule),
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/examples/examples.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { ExamplesComponent } from './examples.component';
+
+
+const routes: Routes = [
+  { path: '', component: ExamplesComponent }
+];
+
+@NgModule({
+  declarations: [
+    ExamplesComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class ExamplesModule { }
